6c93ec90a352ed74d0ebd5b4d9a1acf36954ca4b
JessieCheng <hamy820326@gmail.com>
2020-03-27 20:33:46 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
+const port = 3000
 
 const express = require('express')
 const app = express()
@@ -6,8 +6,13 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 
-const port = 3000
+// 載入 model
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -15,10 +20,20 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-// 載入 model
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
+app.use(session({
+  secret: 'your secret key',
+  resave: 'false',
+  saveUninitialized: 'false',
+}))
+
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 // 使用路由器
 app.use('/users', require('./routes/user'))
